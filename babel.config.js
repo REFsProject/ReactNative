65f5fc78587d136a87a089f0897a24706d0b69a5
Nyrok <47859726+Nyrok@users.noreply.github.com>
2024-01-18 10:51:27 +0100

    Update and Fix Project Packages

--- babel.config.js
+++ babel.config.js
@@ -2,13 +2,5 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      [
-        "@babel/plugin-transform-react-jsx",
-        {
-          "pragma": "h" // Change this to your desired pragma
-        }
-      ]
-    ]
   };
 };
