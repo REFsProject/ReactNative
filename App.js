4fd16bea75e547e0f41af396698fa4937bfc1b8e
Litoz <ilikethebrioche@gmail.com>
2024-01-19 22:08:18 +0100

    Adding React Navigation package

--- App.js
+++ App.js
@@ -1,14 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import {NavigationContainer} from "@react-navigation/native";
+import homePage from "./pages/HomePage";
 
+const Stack = createNativeStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+          <Stack.Navigator initialRouteName= "home">
+              <Stack.Screen name="home" component={homePage}/>
+          </Stack.Navigator>
+          <View style={styles.container}>
+              <Text>Open up App.js to start working on your app! </Text>
+              <StatusBar style="auto" />
+          </View>
+      </NavigationContainer>
   );
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
---
+++ pages/HomePage.js
@@ -0,0 +1,18 @@
+import {SafeAreaView, Text, View} from "react-native";
+import {StyleSheet} from "react-native";
+export default function homePage () {
+    return(
+        <SafeAreaView>
+            <View>
+                <Text>You are on the home page</Text>
+            </View>
+        </SafeAreaView>
+    );
+}
+
+
+
+const styles = StyleSheet.create(
+    {
+
+    })
